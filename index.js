edcf2adc5ea0978632fa622c9fedadc44a700648
Saksham <95493355+clairocoffin@users.noreply.github.com>
2022-09-13 22:08:41 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose');
+const cors = require('cors')
+const userController = require('./controller/user')
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+mongoose.connect(
+  "mongodb://localhost:27017/test",
+  (err) =>
+    err ? console.log(err) : console.log(
+      "Connected to your DB database")
+); 
+
+app.post('/signup', userController.signup)
+app.post('/signin', userController.signin)
+app.post('/submit-otp', userController.submitotp)
+app.post('/send-otp', userController.sendotp)
+
+app.listen(5000, () => {
+    console.log(`Backend Running At Port 5000`)
+})
